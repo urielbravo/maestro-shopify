842a7b0906090a9536765f227444bcec20ecff1e
uriel bravo <uriel.bravo.lara@gmail.com>
2020-06-03 16:04:53 -0700

    axios call to get products

--- client/src/App.js
+++ client/src/App.js
@@ -6,9 +6,60 @@ import ProductSettingsPanel from "./components/ProductSettingsPanel";
 import ProductDisplay from "./components/ProductDisplay";
 import SettingsPanel from "./components/SettingsPanel";
 // import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from 'axios';
+
+
+
+
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      products: []
+    }
+  }
+
+  componentDidMount() {
+    axios.post("https://maestro-store-1.myshopify.com/api/graphql", {
+    query: `{
+      products(first: 100) {
+        edges {
+          node {
+            id
+            title
+            images(first: 1) {
+              edges{
+                node{
+                  id
+                  originalSrc
+                  altText
+                }
+              }
+            }
+          }
+        }
+      }
+    }`,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Shopify-Storefront-Access-Token': 'c58041071dcfc77a86ad631cf6e91633'
+      }
+    })
+    .then(res => {
+      this.setState({
+        products: res.data.data.products.edges
+      });
+    })
+  }
+
+
   render() {
+    {for (const product of this.state.products) {
+      console.log(product.node)
+    }}
     return (
       <div className="App">
         <SettingsPanel />
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Client from 'graphql-js-client';
 import * as serviceWorker from './serviceWorker';
+
+// export const client = new Client(typeBundle, {
+//   url: 'https://graphql.myshopify.com/api/graphql',
+//   fetcherOptions: {
+//     headers: {
+//       'X-Shopify-Storefront-Access-Token': 'dd4d4dc146542ba7763305d71d1b3d38'
+//     }
+//   }
+// });
 
 ReactDOM.render(
   <React.StrictMode>
