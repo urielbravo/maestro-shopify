fa0c4f0958f2f028fbe5cc5d45a4687e8bdce837
Uriel Bravo <uriel.bravo.lara@gmail.com>
2020-06-09 13:46:16 -0700

    already pulling the products of the collections

--- client/src/components/CollectionDisplay.js
+++ client/src/components/CollectionDisplay.js
@@ -1,35 +1,26 @@
 import React from "react";
 import CollectionProduct from "./CollectionProduct";
-import { render } from "react-dom";
+// import { render } from "react-dom";
 
 function CollectionDisplay(props) {
   let collection = props.collections.find((obj) => {
     return obj.node.id === props.collectionID;
   });
 
-  // if (collection) {
-  //   // console.log(`porducts of the collection: ${JSON.stringify(collection.node.products.edges)}`)
-  //   for (const product of collection.node.products.edges) {
-  //     console.log(product);
-  //   }
-  // }
-
   let renderProducts = () => {
     if (collection) {
-      for (const product of collection.node.products.edges) {
-        return (
+      return collection.node.products.edges.map((product) => 
+         (
           <CollectionProduct
-            productImage= {product.node.images.edges[0].node.originalSrc}
-            productId={product.node.id}
-            ProductTitle={product.node.title}
-            productPrice={product.node.variants.edges[0].node.price}
-          />
-        );
-      }
+          productImage={product.node.images.edges[0].node.originalSrc}
+          productId={product.node.id}
+          ProductTitle={product.node.title}
+          productPrice={product.node.variants.edges[0].node.price}
+        />
+        )
+      );
     }
   };
-
-  console.log(collection)
 
   return (
     <div>
@@ -37,9 +28,7 @@ function CollectionDisplay(props) {
         <section className="collection-name-section">
           <h3>FEATURED COLLECTION</h3>
           <p>
-            {collection
-              ? collection.node.title
-              : "just another colletction"}
+            {collection ? collection.node.title : "just another colletction"}
           </p>
         </section>
         <section className="collection-products-section">
